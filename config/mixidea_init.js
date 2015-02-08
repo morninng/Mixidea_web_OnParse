0766ba98565e22cd6c0b330764080b2881d2d7a9
morninng <mr_morninng_star_dash@yahoo.co.jp>
2015-02-09 00:45:06 +0900

    facebook and parse setting works

---
+++ config/mixidea_init.js
@@ -0,0 +1,13 @@
+
+  Parse.initialize("rc8Zz9FHNj4FVgabkbjRPbPrlLXQQpoL9NRrI7FZ", "fYVcXHYQqvSLRMmRQ274PEmmUfj5OnwTHrRwXlNm");
+  var config_hangout_app_id_Registration = "51676125349";
+  var config_hangout_app_id_Debate_NA = "51676125349";
+  var config_hangout_app_id_Debate_BP = "51676125349";
+  var config_hangout_app_id_Debate_Asian = "51676125349";
+  var config_hangout_app_id_Debate_Austral = "51676125349";
+  var config_hangout_app_id_Discussion = "51676125349";
+  var config_hangout_app_id = "172012775158";
+  var default_hangout_url = "https://plus.google.com/hangouts/_";
+  var facebook_app_id = "1524156424491576";
+  console.log("parse init");
+
